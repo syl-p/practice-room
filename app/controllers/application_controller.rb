cee208e5358fda2b0f13e3ea337ac1cf040da99e
sylv-p <sylvain@atnos.com>
2022-01-09 11:53:06 +0100

    feat: add bio to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :avatar)}
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :avatar)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :avatar, :bio)}
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,6 +15,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
   def user_params
-    params.require(:user).permit(:current_password, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:current_password, :password, :password_confirmation, :avatar, :bio, :username)
   end
 end
